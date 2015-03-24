035925d229bc5cfb93b676f602cb279598a7b37e
JEONGHWAN <adachi21@gmail.com>
2015-03-24 09:19:36 +0900

    Create intent.java

---
+++ intent.java
@@ -0,0 +1,5 @@
+Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:010-0000-0000"));
+startActivity(intent);
+
+Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-0000-0000"));
+startActivity(intent);
