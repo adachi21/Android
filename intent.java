4831aeb8df4c7ffaf64c1f5bca0aef62792b7c43
JEONGHWAN <adachi21@gmail.com>
2015-03-24 09:28:23 +0900

    Update intent.java

--- intent.java
+++ intent.java
@@ -3,3 +3,9 @@ startActivity(intent);
 
 Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:010-0000-0000"));
 startActivity(intent);
+
+Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+startActivity(intent);
+
+Intent intent = new Intent(getApplicatonContext(), LaunchedActivity.class);
+startActivity(intent);
